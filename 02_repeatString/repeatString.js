5947f58ae828892b6dada3e48d3e3fa720933ac9
Guwayria <guwayriam@gmail.com>
2022-03-22 14:13:38 -0700

    completed the first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+function repeatString(string, num) {
+    word= '';
+    if(num < 0){
+       return 'ERROR'
+    }else{
+      for (let i = 0; i < num; i++ ) {
+         word += string;
+     }
+     return word;
+    }
+       
+    
+}
 
 // Do not edit below this line
 module.exports = repeatString;
