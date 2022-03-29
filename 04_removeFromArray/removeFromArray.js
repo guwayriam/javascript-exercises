6c6063260be03dd5726d94f722ea55dd865a90a9
Guwayria <guwayriam@gmail.com>
2022-03-28 20:05:08 -0700

    remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,30 @@
-const removeFromArray = function(list, num) {
-    location = list.indexOf(num);
-    list.splice(location, num);
+const removeFromArray = function(list) {
+
+    let remove = Array.from(arguments);
+    remove.shift();
+
+    for(let i = 0; i < remove.length; i++) {
+        let element = remove[i];
+        let location = list.indexOf(element);
+        if(location > -1){
+            list.splice(location, 1);
+        }
+        
+    }
     return list;
+    
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+/*
+  for(let i = 0; i < num.length; i++) {
+        let element = num[i];
+        location = list.indexOf(element);
+        list.splice(location, 1);
+    }
+   /* let location = list.indexOf(num);
+    list.splice(location, 1);*/
+    //return list;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let min = Math.min(first, second);
+    let max = Math.max(first, second);
 
+    let total = 0;
+
+    if(min >= 0){
+        for (let i = min; i <= max; i++) {
+            total += i;
+        }
+        return total;
+
+    }else{
+        return 'ERROR'
+    }
+
+    
+
+    
 };
 
 // Do not edit below this line
