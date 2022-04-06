5d290beb575fb4879d87e33fa00aaf9655cc1654
Guwayria <guwayriam@gmail.com>
2022-04-06 11:11:03 -0700

    one more done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,16 +1,28 @@
-const fibonacci = function(num) {
-    let sequence = [];
-    for (let i = 1; i < num; i++) {
-        if (sequence.length < 2){
-            sequence.push(1);
-        }else{
-            let add = sequence[i-2] + sequence[i-1]
+const fibonacci = function(n) {
+    let sequence = [0,1];
+    
+    if(n < 0){
+        return 'OOPS'
+
+    }else if(n === 1){
+        return sequence[0];
+
+    }else if(n === 2){
+        return sequence[1];
+
+    }else{
+        for(let i = 2; i <= n; i++){
+            //console.log(sequence)
+            let add = sequence[sequence.length-1] + sequence[sequence.length-2]
+            //console.log(add)
             sequence.push(add);
         }
-    }
 
+    }
+    let num = parseInt(n);
     return sequence[num];
 };
 
+//console.log(fibonacci('3'));
 // Do not edit below this line
 module.exports = fibonacci;
