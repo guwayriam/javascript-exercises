9ac48b8a5eb15576bf85e85b89b8b81ff7286fe0
Guwayria <guwayriam@gmail.com>
2022-04-11 12:02:22 -0700

    done.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let word = str.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase();
+    string = str.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase();
+    let reverseString = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+        reverseString += word.charAt(i);
+    } 
+        
+    //console.log(reverseString);
+    //console.log(string)
 
+    let bool = true;
+    let i = 0;
+    while(bool && i < string.length){
+        if (reverseString.charAt(i) === string.charAt(i)){
+            //console.log(reverseString[i] + "   " + string[i])
+            bool = true;
+            i++
+        }else{
+            return false;
+        }
+    }
+    //console.log(bool)
+    
+    return bool;
 };
-
+console.log(palindromes('Racecar!'))
 // Do not edit below this line
 module.exports = palindromes;
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let tital = books.map(function(element){
+       // console.log(element.title)
+        return element.title;
+    })
+    //console.log(tital)
+    return tital;
 };
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
